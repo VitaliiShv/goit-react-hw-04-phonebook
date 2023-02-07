93b64b0c588ab6795cfb7a463191362df8b79aa6
VitaliiShv <vitalyashved@gmail.com>
2023-02-07 17:35:15 +0200

    redo to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,80 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 
-export class App extends Component {
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    return contacts ? contacts : [];
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const isDuplicate = name => {
+    if (contacts.find(contact => contact.name === name)) {
+      alert(`${name} is already in contact list`);
+      return;
+    }
+  };
+
+  const addContact = ({ name, number }) => {
+    isDuplicate(name);
+
+    setContacts(prevContacts => {
+      const contact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+
+      return [contact, ...prevContacts];
+    });
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  const changeFilter = ({ target }) => setFilter(target.value);
+
+  const getVisibleContacts = () => {
+    if (!filter) {
+      return contacts;
+    }
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const visibleContacts = getVisibleContacts();
+
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      {contacts.length > 0 && (
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={deleteContact}
+        />
+      )}
+    </>
+  );
+};
+
+export default App;
+/*
+class App extends Component {
   state = {
     contacts: [],
     filter: '',
@@ -79,3 +149,6 @@ export class App extends Component {
     );
   }
 }
+
+export default App;
+*/
